26186384969e87ea91ee811748b718326f6103be
Daniela Beltran <dpbeltran97@gmail.com>
2019-04-16 16:54:47 -0500

    verificación
    miro si funcoinan las migraciones todo super ok

--- Codigo/homePage/models.py
+++ Codigo/homePage/models.py
@@ -84,4 +84,3 @@ class Compradores(models.Model):
 		libro = models.ForeignKey(infoLibro, on_delete=models.SET_NULL,null=True)
 		usuarioDuenio=models.ForeignKey (infousuario,on_delete=models.SET_NULL, null=True,related_name='usuarioDuenio')
 		usuarioComprador=models.ForeignKey (infousuario,on_delete=models.SET_NULL, null=True,related_name='usuarioComprador')
-
